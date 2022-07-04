d3d55ae1bb659d8fd04cd7c6307781c3b0ee41cf
Cristhian Buiza <cbuiza@ucvvirtual.edu.pe>
2022-07-03 23:28:12 -0500

    Map funcionality

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,32 @@
 import React from "react";
-
+import useStyles from "./styles.js";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+import "./styles.js";
 const List = () => {
-  return <div>List</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurantes, Hoteles, Atracciones turisticas
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel value="" onChange={""}>
+          Type
+        </InputLabel>
+        <Select value="" onChange={""}>
+          <MenuItem value="restaurants">Restaurantes</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 
 export default List;
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlined from "@material-ui/icons/LocationOnOutlined";
+import { Rating } from "@material-ui/lab";
+import useStyles from "./styles";
 
 const Map = () => {
-  return <div>Map</div>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const coordinates = { lat: 0, lng: 0 };
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyAtj50KxmIGJFF98b6oOZbLbxVFOyF7XQA" }}
+        defaultCenter={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
